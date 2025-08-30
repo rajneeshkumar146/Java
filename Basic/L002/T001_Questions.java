b2ffc7c012637b7397b64e6e7c746769fa68e3b7
Rajneesh <rajneeshkumar146@gmail.com>
2025-08-31 01:50:18 +0530

    update

--- Basic/L002/T001_Questions.java
+++ Basic/L002/T001_Questions.java
@@ -5,6 +5,41 @@ public class T001_Questions {
 
     }
 
-    
-    
+    public static int rotateNumber(int N, int K) {
+        int d = countDigit(N);
+        K = (K % d + d) % d;
+
+        int divisor = (int) Math.pow(10, d - K);
+        int multipler = (int) Math.pow(10, K);
+
+        int quo = N / divisor;
+        int rem = N % divisor;
+
+        return quo * multipler + rem;
+    }
+
+    public static int reverseNumber(int N) {
+        int ans = 0;
+        while (N != 0) {
+            int rem = N % 10;
+            N /= 10;
+
+            ans = ans * 10 + rem;
+        }
+        return ans;
+    }
+
+    public static int countDigit(int N) {
+        int count = 0;
+        while (N != 0) {
+            N /= 10;
+            count++;
+        }
+        return count;
+    }
+
+    public static boolean isEven(int N) {
+        return N % 2 == 0;
+    }
+
 }
--- Basic/L002/T002_Arrays.java
+++ Basic/L002/T002_Arrays.java
@@ -1,8 +1,80 @@
 package Java.Basic.L002;
 
+import java.util.*;
+
 public class T002_Arrays {
+
+    public static Scanner scn = new Scanner(System.in);
+
     public static void main(String args[]) {
 
+        int[] arr = { 1, 3, 4, 4, 5, 6, 7, 5, 8, 5, 7, 5, 10, 11 };
+    }
+
+    public static int findLastIndex(int[] arr, int number) {
+        int lastIndex = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == number) {
+                lastIndex = i;
+            }
+        }
+        return lastIndex;
+    }
+
+    // Last baar number kaunse index pe mila hai,
+    public static int findLastIndex2(int[] arr, int number) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] == number) {
+                return i;
+            }
+        }
+        return -1; // Number exist nahi krta hai, isliye uska index -1.
+    }
+
+    // Pheli baar number kaunse index pe mila hai,
+    public static int findFirstIndex(int[] arr, int number) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == number) {
+                return i;
+            }
+        }
+        return -1; // Number exist nahi krta hai, isliye uska index -1.
+    }
+
+    // FindElement
+    public static boolean isNumberPresent(int[] arr, int number) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int findOccurence(int[] arr, int occurenceNumber) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == occurenceNumber) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // helper Method. ------------------------------
+
+    public static void createArrayUsingCustomInput() {
+        int N = scn.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 
 }
