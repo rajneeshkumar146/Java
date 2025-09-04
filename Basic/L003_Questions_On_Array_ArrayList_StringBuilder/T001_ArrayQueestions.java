cb4510dc5cd437c3b11467dac13e6771fe029233
Rajneesh <rajneeshkumar146@gmail.com>
2025-09-04 10:21:56 +0530

    update

--- Basic/L003_Questions_On_Array_ArrayList_StringBuilder/T001_ArrayQueestions.java
+++ Basic/L003_Questions_On_Array_ArrayList_StringBuilder/T001_ArrayQueestions.java
@@ -2,9 +2,76 @@ package L003_Questions_On_Array_ArrayList_StringBuilder;
 
 public class T001_ArrayQueestions {
     public static void main(String args[]) {
-
+        int[] arr = { 1, 2, 3, 3, 3, 4, 4, 4, 4, 5, 6 };
+        arr[3] = 100;
+        // System.out.println("Idx: " + removeDuplicate(arr));
     }
 
+    // Question: Remove duplicate from the sorted array.
+    // [1,2,3,3,3,4,4,4,4,5,6] -> idx: 5, [1,2,3,4,5,6,-,-,-,-,-]
+    public static int removeDuplicate(int[] arr) {
+        if (arr.length == 0) {
+            return -1;
+        }
 
-    
+        // sidx: starting idx.
+        int sidx = 0;
+        int itr = sidx + 1;
+        while (itr < arr.length) {
+            if (arr[sidx] != arr[itr]) {
+                // sidx++;
+                swap(arr, ++sidx, itr);
+            }
+            itr++;
+        }
+
+        for (int val : arr)
+            System.out.print(val + " ");
+
+        return sidx;
+    }
+
+    // Check array is sorted,
+    // [1,1,2,2,3,3] -> true
+    // [1,2,1,2,3,3] -> false
+    public static boolean checkArrayIsSorted(int[] arr) {
+        if (arr.length <= 1) {
+            return true;
+        }
+
+        int sidx = 1;
+        while (sidx < arr.length) {
+            if (arr[sidx - 1] > arr[sidx]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // Rotate array by k
+    // [a,b,c,d,e,f,g,h], 3 -> [d,e,f,g,h,a,b,c]
+    public static void rotateArrayByK(int[] arr, int k) {
+        if (arr.length <= 1) {
+            return;
+        }
+
+        reverseArray(arr, 0, arr.length - 1);
+        reverseArray(arr, arr.length - k, arr.length - 1);
+        reverseArray(arr, 0, arr.length - k - 1);
+    }
+
+    // Helper -------------------------------------------
+
+    public static void reverseArray(int[] arr, int sidx, int eidx) {
+        while (sidx <= eidx) {
+            swap(arr, sidx++, eidx--);
+        }
+    }
+
+    public static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
 }
---
+++ Basic/L003_Questions_On_Array_ArrayList_StringBuilder/T002_ArrayList.java
@@ -0,0 +1,124 @@
+package L003_Questions_On_Array_ArrayList_StringBuilder;
+
+import java.util.*;
+
+public class T002_ArrayList {
+    public static Scanner scn = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // ArrayList<Integer> arrList = intputInArrayList();
+        // System.out.println(arrList);
+
+        // System.out.println("get: " + arrList.get(2));
+        // arrList.remove(2);
+        // System.out.println(arrList);
+        // System.out.println(arrList.size());
+
+        // int[] arr = { 1, 2, 3, 4, 4, 4, 4, 5, 6 };
+        // int[] ans = removeAllDuplicates(new int[] { 1, 2, 3, 4, 4, 4, 4, 5, 6 });
+        // for (int val : ans)
+        //     System.out.print(val + " ");
+
+
+        System.out.println(getAllPrimeNumbers(50));
+    }
+
+    /**
+     * TimeComplexity:
+     * get, add, size: O(1)
+     * remove: O(N)
+     * if remove if olny for last index: O(1)
+     * 
+     * @return
+     */
+
+    public static ArrayList<Integer> intputInArrayList() {
+        ArrayList<Integer> arrList = new ArrayList<>();
+        int N = scn.nextInt();
+        for (int i = 0; i < N; i++) {
+            arrList.add(scn.nextInt());
+        }
+
+        return arrList;
+    }
+
+    public static int[] removeAllDuplicates(int[] arr) {
+        if (arr.length <= 1) {
+            return arr;
+        }
+
+        Arrays.sort(arr);
+        ArrayList<Integer> arrList = new ArrayList<>();
+
+        int sidx = 0;
+        arrList.add(arr[sidx++]);
+
+        while (sidx < arr.length) {
+            // if(arrList.get(arrList.size() - 1) != arr[sidx]){
+            // arrList.add(arr[sidx]);
+            // }
+            if (arrList.getLast() != arr[sidx]) {
+                arrList.add(arr[sidx]);
+            }
+            sidx++;
+        }
+
+        int[] res = new int[arrList.size()];
+        for (int i = 0; i < arrList.size(); i++) {
+            res[i] = arrList.get(i);
+        }
+
+        return res;
+    }
+
+    // T: O(2N)
+    public static int[] getAllIndex(int[] arr, int data) {
+        int count = 0;
+        for (int val : arr) {
+            if (val == data)
+                count++;
+        }
+
+        int[] indexArray = new int[count];
+        int sidx = 0;
+        for (int idx = 0; idx < arr.length; idx++) {
+            if (arr[idx] == data) {
+                indexArray[sidx++] = data;
+            }
+        }
+
+        return indexArray; // to return empty array.
+    }
+
+    // T: O(N)
+    public static ArrayList<Integer> getAllIndex2(int[] arr, int data) {
+        ArrayList<Integer> res = new ArrayList<>();
+        for (int idx = 0; idx < arr.length; idx++) {
+            if (arr[idx] == data) {
+                res.add(idx);
+            }
+        }
+
+        return res; // to return empty array.
+    }
+
+    // T: O(NSqrt(N))
+    public static ArrayList<Integer> getAllPrimeNumbers(int N) {
+        ArrayList<Integer> res = new ArrayList<>();
+        for (int num = 2; num <= N; num++) {
+            boolean isPrime = true;
+            for (int divisor = 2; divisor <= (int) Math.sqrt(num); divisor++) {
+                if (num % divisor == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                res.add(num);
+            }
+        }
+        return res;
+    }
+
+}
